42c33de1ee8dfec4eecc83b85d15ca55f4f71841
wenchloe <33583236+wenchloe@users.noreply.github.com>
2018-01-20 13:30:12 -0800

    Update NexisWebscraper.java

--- NexisWebscraper.java
+++ NexisWebscraper.java
@@ -13,15 +13,7 @@
  * */
 
 /*
- * Pre: Download Apache POI zip files, Commons Collection, geckodriver, and Selenium WebDriver
- * 		Configure build path in Eclipse - add external jars: 
- * 			- Apache POI: poi-3.17.jar
- * 			- Commons Collection: commons-codec-.1.10.jar, commons-collections4-4.1-javadoc.jar,
- * 								  commons-collections4-4.1.jsar,commons-exec-1.3.jar, 
- * 								  commons-logging-1.2.jar
- * 			- Selenium WebDriver: selenium standalone server, client-combined-3.8.1.jar, 
- * 								  client-combined-3.8.1-sources.jar
- * 
+ * See ReadMe for configuration details / list of packages and jars needed to be added 
  * */
 
 // import Util and SimpleDateFormat (for Scanners + filtering irrelevant articles)
